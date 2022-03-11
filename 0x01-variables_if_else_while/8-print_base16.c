d5e087bc695975968e06bf6b96b321690f9fc3e8
Anisah-hash <anysahani125@gmail.com>
2022-03-11 09:10:59 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 	int n;
 	char l;
 
-	for (n =0; n <10; n++)
+	for (n =0; n < 10; n++)
 	{
 		putchar(n);
 	}
-	for(l = 'a'; l <='f'; l++)
+	for(l = 'a'; l <= 'f'; l++)
 	{
 		putchar(l);
 	}
