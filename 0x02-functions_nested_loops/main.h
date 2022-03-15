588a69546f29615b0b6be51d13fdfa709b7c5445
Anisah-hash <anysahani125@gmail.com>
2022-03-15 03:15:50 -0700

    another commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+* main - prints a string
+* Description: the string is "_putchar"
+*
+* Return: 0
+*/
+
+int main(void)
+{
+
+        putchar('_');
+        putchar('p');
+        putchar('u');
+        putchar('t');
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+
+        return(0);
+
