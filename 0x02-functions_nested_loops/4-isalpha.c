cb924d494417495efcfe8129da82ff7f0e3299d6
Anisah-hash <anysahani125@gmail.com>
2022-03-15 09:44:16 -0700

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/** _isalpha checks for alphabetic letters
+ * @c: is the letter to be checked
+ * Return: 1 if c is a letteer, 0 if otherwise
+ */
+int _isalpha(int c);
+{
+	return(c >= 'a' && c <= 'z');
+	return(c >= 'A' && c <='Z');
+}
