fc1554533267b3d11bde86fbff4e65f9898e972f
Anisah-hash <anysahani125@gmail.com>
2022-03-21 10:12:16 -0700

    new

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,15 +6,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void swap_int(int *a, int *b)
 {
-    int a;
-    int b;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+	int swap;
+	swap = *b;
+	*b = *a;
+	*a = swap;
+
 }
