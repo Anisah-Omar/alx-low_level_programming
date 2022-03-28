65f7ce38e405566ddd90140caff6507f822e497e
Anisah-hash <anysahani125@gmail.com>
2022-03-28 12:13:15 -0700

    a function that locates a character in a string.
    Prototype: char *_strchr(char *s, char c);
    Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,22 @@
 #include "main.h"
-#include <stddef.h>
+#define NULL 0
 
 /**
- * _strchr - locates char in string
- * @s: pointer to string
- * @c: char to locate
- *
- * Return: pointer to first occurence of c in s
- */
+*   _strchr - locate 1st occurrence of char in string and returns pointer there
+*    @s: string to search
+*     @c: target characer
+*      Return: pointer to that character in string
+*/
+
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-	return (!c ? s : NULL);
+int i = 0;
+
+while (s[i] != '\0' && s[i] != c)
+i++;
+
+if (s[i] == c)
+return (&s[i]);
+else
+return (NULL);
 }
