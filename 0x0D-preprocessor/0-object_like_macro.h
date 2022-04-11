2fe5e1563373643ae9310dcbff0732f13a6b6369
Anisah Omar <71156452+Anisah-Omar@users.noreply.github.com>
2022-04-11 15:55:24 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,9 @@
-#ifndef _0_OBJECT_LIKE_MACRO_H_
+#ifndef OBJECT_LIKE_MICRO_H
+
 /**
- * description : header file that defines a macro named SIZE as an abbreviation from the token 1024
- *
+ * description: header file that defines a macro named SIZE as an abbreviation for the token 1024.
+ * 
  */
-#define _0_OBJECT_LIKE_MACRO_H_
 #define SIZE 1024
+
 #endif
